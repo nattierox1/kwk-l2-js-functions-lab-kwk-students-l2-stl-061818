58afcc466b29067bb6d3edcc98507be04f3bf434
nattierox1 <nattierox1@gmail.com>
2018-06-22 21:06:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ function calculaltesFarePrice(startingBlock, endingBlock){
   let feet = distanceTravelledInFeet(startingBlock,endingBlock);
   if (feet <= 400) {
     return"Here's a free sample.";
-  }else if (400 < feet && feet <= 2000)
+  } else if (400 < feet && feet <= 2000)
   return Math.abs((feet - 400) * 0.02);
-}else if (2000 < feet && feet <= 2500)
+} else if (2000 < feet && feet <= 2500)
 return 25; 
-}else if (feet> 2500); {
+} else if (feet> 2500); {
   return "cannot travel that far"
 }
 
