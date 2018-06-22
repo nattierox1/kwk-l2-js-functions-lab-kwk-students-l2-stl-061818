27258038fec87983d6fb578a13596febdc1ec24e
nattierox1 <nattierox1@gmail.com>
2018-06-22 20:54:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (street) {
+ let blocks = street -42;
+ return Math.abs(blocks);
+}
+
+function distanceFromHqInFeet (street) {
+  let block = street - 42;
+  let feet = blocks * 264; 
+  return Math.abs(feet);
+}
+
+
+function distanceTravelledInFeet (startingBlock, endingBlock) {
+ let streetDif = endingBlock - startingBlock;
+ let feet = streetDif * 264;
+ 
+ return Math.abs(feet);
+}
+
+function calculaltesFarePrice(startingBlock, endingBlock){
+  if (feet <= 400); {
+    return"Free sample.";
+  }
+}
+
+else if (400 < feet && feet < 2000) {
+  return Math.abs(feet * 0.02);
+  
+}
+
+
